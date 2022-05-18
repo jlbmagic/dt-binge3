69532bc6a670176fd9efd422572240ba4ee65569
Jeremy Brown <jlbmagic@gmail.com>
2022-05-18 14:57:05 -0400

    got DT up and running.

---
+++ src/columns.js
@@ -0,0 +1,20 @@
+export const customerColumns = [
+  { title: "City", data: "fieldData.City" },
+  { title: "Co", data: "fieldData.CompanyName" },
+  {
+    title: "State",
+    data: "fieldData.State",
+  },
+];
+export const admissionsColumns = [
+  { title: "Status", data: "fieldData.Status" },
+  { title: "Category", data: "fieldData.Category" },
+  {
+    title: "Grade",
+    data: "fieldData.Grade",
+  },
+  {
+    title: "Region",
+    data: "fieldData.Region",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 //Here we're importing items we'll need. You can add other imports here.
-
+import { customerColumns, admissionsColumns } from "./columns";
 var table;
 
 // exposing loadData to FileMaker Script
 window.loadData = function (json) {
-  // TODO: Load the DataTables libraries by linking to the DataTables CDN.
-  //TODO: Get data from FileMaker
-  //TODO: prep it for the JS.
+  const obj = JSON.parse(json);
+  const data = obj.data;
+  const dataType = obj.columnType;
+  // return;
 
-  //TODO: create the columns.
+  const columns =
+    dataType === "Customers" ? customerColumns : admissionsColumns;
 
-  //TODO: Explore options of Datatables
   //https://datatables.net/reference/option/
   //https://datatables.net/examples/index
   // Create the DataTable, after destroying it if already exists
   table = $("#dtable").DataTable({
     paging: true,
-    pageLength: 20,
-    searching: true,
+    pageLength: 10,
+    searching: false,
     colReorder: true,
-    columns: [],
-    data: [],
+    order: [[2, "desc"]],
+    columns: columns,
+    data: data,
   });
 };
